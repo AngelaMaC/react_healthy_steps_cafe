fb27939b28b78b12814397e8dd3b68163e2124f9
Angela Canning <amc@angelacanning.com>
2020-01-17 11:17:56 -0600

    Changing Functional Components

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,42 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
-import MenuInfo from './MenuInfoComponent';
 
-class Directory extends Component {
-    constructor(props) {
-		super(props);
-		this.state = {
-			selectedMenu: null
-		};
-    }
-    
-    onMenuSelect(menu) {
-		this.setState({ selectedMenu: menu });
-	}
+function RenderDirectoryItem({menu, onClick}) {
+    return (
+        <Card onClick={() => onClick(menu.id)}>
+            <CardImg width="100%" src={menu.image} alt={menu.name} />
+            <CardImgOverlay>
+                <CardTitle>{menu.name}</CardTitle>
+            </CardImgOverlay>
+    </Card>
+    );
+}
 
-    render() {
-        const directory = this.props.menus.map(menu => {
-            return (
-                <div key={menu.id} className="col-md-5 m-1">
-                    <Card onClick={() => this.props.onClick(menu.id)}>
-                        <CardImg width="100%" src={menu.image} alt={menu.name} />
-                        <CardImgOverlay>
-                            <CardTitle>{menu.name}</CardTitle>
-                        </CardImgOverlay>
-                    </Card>
-                </div>
-            );
-        });
-
+function Directory (props) {
+    const directory = props.menus.map(menu => {
         return (
-            <div className="container">
-                <div className="row">
-                    {directory}
-                </div>
-                <MenuInfo menu={this.state.selectedMenu} />
+            <div key={menu.id} className="col-md-5 m-1">
+                <RenderDirectoryItem menu={menu} onClick={props.onClick} />
             </div>
         );
-    }
+    });
+
+    return (
+        <div className="container">
+            <div className="row">
+                {directory}
+            </div>
+        </div>
+    );
 }
 
 export default Directory;
--- src/components/MenuInfoComponent.js
+++ src/components/MenuInfoComponent.js
@@ -1,53 +1,53 @@
-import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import React from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
+function RenderMenu({menu}) {
+    return (
+        <div className="col-md-5 m-1">
+                <Card>
+                    <CardImg top src={menu.image} alt={menu.name} />
+                    <CardBody>
+                        <CardTitle>{menu.name}</CardTitle>
+                        <CardText>{menu.description}</CardText>
+                    </CardBody>
+                </Card>
+        </div>
+    );
+}
 
-class MenuInfo extends Component {
-	renderMenu(menu) {
+function RenderComments({comments}) {
+    if (comments) {
         return (
             <div className="col-md-5 m-1">
-					<Card onClick={() => this.onMenuSelect(menu)}>
-						<CardImg width="100%" src={menu.image} alt={menu.name} />
-						<CardImgOverlay>
-							<CardTitle>{menu.name}</CardTitle>
-						</CardImgOverlay>
-					</Card>
-        </div>
+                <h4>Comments</h4>
+                { comments.map(comment => {
+                    return (
+                        <div key={comment.id}>
+                            <p>{comment.text} <br />
+                            {comment.author}, -- {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                        </div>
+                    )
+                })}
+            </div>
         );
     }
-
-        renderComments(comments) {
-            if (comments) {
-                return (
-                    <div className="col-md-5 m-1">
-                        <h4>Comments</h4>
-                        { comments.map(comment => {
-                            return (
-                                <div key={comment.id}>
-                                    <p>{comment.text} <br />
-                                    {comment.author}, -- {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
-                                </div>
-                            )
-                        })}
-                    </div>
-                )
-            }    
-        }
+    return <div />;    
+}
 
 
-        render() {
-            if (this.props.menu) {
-                return (
-                    <div className="Container">
-                        <div className="Row">
-                        { this.renderMenu(this.props.menu) }
-                        {this.renderComments(this.props.menu.comments)}
-                        </div>
-                    </div>
-                );
-            }
-            return <div />;
-        }
+function MenuInfo(props) {
+    if (props.menu) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <RenderMenu menu={props.menu} />
+                    <RenderComments comments={props.menu.comments} />
+                </div>
+            </div>
+        );
     }
+    return <div />;
+}
+    
 
 export default MenuInfo;
